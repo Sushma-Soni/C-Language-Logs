b902d94c1d27a28e2bc2123d1eb9c69a86a4f1a3
Sushma <sushmasoni1351@gmail.com>
2024-07-25 17:01:28 +0530

    Addition
    simple addition of two numbers.

---
+++ Add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+/*
+    // Write C code here
+    int num1;
+    int num2;
+    printf("Enter the first no:-");
+    scanf("%d",&num1);
+    printf("Enter second no:- ");
+    scanf("%d",&num2);
+    
+    printf("result:%d",num1+num2);
+*/
+    double num1;
+    double num2;
+    printf("Enter the first no:-");
+    scanf("%lf",&num1);
+    printf("Enter second no:- ");
+    scanf("%lf",&num2);
+    
+    printf("result:%f",num1+num2);
+    return 0;
+}
+
+
